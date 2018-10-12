16f4c80b7416cd869667f4247e006a8786e321c2
Chianti Scarlett <chianti.scarlett@gmail.com>
2018-10-12 14:20:37 +0900

    Conceiving basic framework

---
+++ matrix.py
@@ -0,0 +1,13 @@
+class Matrix:
+    def __init__(self):
+        pass
+
+
+class SquareMatrix(Matrix):
+    def __init__(self):
+        pass
+
+
+class AugmentedMatrix(Matrix):
+    def __init__(self):
+        pass
